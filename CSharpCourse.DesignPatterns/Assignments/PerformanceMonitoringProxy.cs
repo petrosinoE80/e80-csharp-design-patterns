b6b461fa12e16c2c495899596c55dafb0064e632
Antonio Petrosino <petrosino.a@e80group.com>
2024-11-04 15:54:47 +0100

    tested

--- CSharpCourse.DesignPatterns/Assignments/PerformanceMonitoringProxy.cs
+++ CSharpCourse.DesignPatterns/Assignments/PerformanceMonitoringProxy.cs
@@ -6,7 +6,9 @@ namespace CSharpCourse.DesignPatterns.Assignments;
 internal interface IService
 {
     void DoSomething();
+
     Task DoSomethingAsync();
+
     Task<bool> GetResultAsync();
 }
 
@@ -35,21 +37,35 @@ internal class SlowService : IService
         return true;
     }
 }
+
 internal class PerformanceMonitoringProxy<T> : DispatchProxy where T : class
 {
-    private T _decorated;
+    private T? _decorated;
     private TimeSpan _threshold;
 
-    public static T? Create(T decorated, TimeSpan threshold)
+    private async Task HandleTask(Task task, MethodInfo targetMethod, Stopwatch stopwatch)
     {
-        // Crea un'istanza del proxy come PerformanceMonitoringProxy<T>
-        var proxy = Create<T, PerformanceMonitoringProxy<T>>() as PerformanceMonitoringProxy<T> ?? throw new FormatException();
+        await task;
+        RaiseStopWatch(targetMethod, stopwatch);
+    }
 
-        // Assegna i campi decorati e soglia al proxy
-        proxy._decorated = decorated;
-        proxy._threshold = threshold;
+    private async Task<TResult> HandleTaskWithResult<TResult>(Task<TResult> task, MethodInfo targetMethod, Stopwatch stopwatch)
+    {
+        TResult result = await task;
+        RaiseStopWatch(targetMethod, stopwatch);
+        return result;
+    }
 
-        return proxy as T;
+    private void RaiseStopWatch(MethodInfo? targetMethod, Stopwatch stopwatch)
+    {
+        stopwatch.Stop();
+
+        if (targetMethod == null) throw new FormatException();
+
+        if (stopwatch.Elapsed > _threshold)
+        {
+            Console.WriteLine($"Method {targetMethod.Name} took {stopwatch.ElapsedMilliseconds}ms, exceeding the threshold of {_threshold.TotalMilliseconds}ms.");
+        }
     }
 
     protected override object? Invoke(MethodInfo? targetMethod, object?[]? args)
@@ -57,30 +73,31 @@ internal class PerformanceMonitoringProxy<T> : DispatchProxy where T : class
         if (targetMethod == null) throw new FormatException();
 
         var stopwatch = Stopwatch.StartNew();
-
-        // Controlla se il metodo restituisce un Task
         var result = targetMethod.Invoke(_decorated, args);
 
+        //if it is async
         if (result is Task task)
         {
-            // Controlla se il Task ha un tipo di risultato
             var taskType = task.GetType();
-            if (taskType.IsGenericType && taskType.GetGenericTypeDefinition() == typeof(Task<>))
+            if (taskType.IsGenericType && task.GetType().GetProperty("Result", BindingFlags.Public | BindingFlags.Instance) != null)
             {
-                // Gestisci il Task con risultato
+                //HandleTaskWithResult
                 var resultType = taskType.GetGenericArguments()[0];
-                var handleMethod = typeof(PerformanceMonitoringProxy<T>).GetMethod(nameof(HandleTaskWithResult), BindingFlags.NonPublic | BindingFlags.Instance)
+                var handleMethod = typeof(PerformanceMonitoringProxy<T>).GetMethod(nameof(HandleTaskWithResult),
+                    bindingAttr: BindingFlags.NonPublic | BindingFlags.Instance)
                     ?.MakeGenericMethod(resultType);
 
-                return handleMethod?.Invoke(this, new object[] { task, targetMethod, stopwatch });
+                return handleMethod?.Invoke(this, [task, targetMethod, stopwatch]);
             }
             else
             {
-                // Gestisci il Task senza risultato
+                //HandleTaskWithoutResult 
                 return HandleTask(task, targetMethod, stopwatch);
             }
         }
 
+
+        // if it is not async
         stopwatch.Stop();
         if (stopwatch.Elapsed > _threshold)
         {
@@ -90,34 +107,11 @@ internal class PerformanceMonitoringProxy<T> : DispatchProxy where T : class
         return result;
     }
 
-
-    private void RaiseStopWatch(MethodInfo? targetMethod, Stopwatch stopwatch)
+    public static T? Create(T decorated, TimeSpan threshold)
     {
-        stopwatch.Stop();
-
-        if(targetMethod == null) throw new FormatException();
-
-        if (stopwatch.Elapsed > _threshold)
-        {
-            Console.WriteLine($"Method {targetMethod.Name} took {stopwatch.ElapsedMilliseconds}ms, exceeding the threshold of {_threshold.TotalMilliseconds}ms.");
-        }
-    }
-
-    private async Task HandleTask(Task task, MethodInfo targetMethod, Stopwatch stopwatch)
-    {
-        await task; 
-        RaiseStopWatch(targetMethod, stopwatch);
-    }
-
-    private async Task<TResult> HandleTaskWithResult<TResult>(Task<TResult> task, MethodInfo targetMethod, Stopwatch stopwatch)
-    {
-        // Aspetta il completamento del Task e ottieni il risultato
-        TResult result = await task;
-
-        // Al termine dell'operazione, ferma il cronometro
-        RaiseStopWatch(targetMethod, stopwatch);
-
-        // Restituisci il risultato
-        return result;
+        var proxy = Create<T, PerformanceMonitoringProxy<T>>() as PerformanceMonitoringProxy<T> ?? throw new FormatException();
+        proxy._decorated = decorated;
+        proxy._threshold = threshold;
+        return proxy as T;
     }
 }
